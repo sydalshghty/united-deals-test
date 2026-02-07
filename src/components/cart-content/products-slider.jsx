0139e82b4e150ff5ecb560f89bea7051e675aa7a
Saeed Mohamed <sydalshghty15@gmail.com>
2026-02-07 14:56:27 +0200

    upload-cart-page-products-slider

--- src/components/cart-content/products-slider.jsx
+++ src/components/cart-content/products-slider.jsx
@@ -1,38 +1,102 @@
 import { FaCaretLeft, FaCaretRight } from "react-icons/fa6";
 import { FiShoppingCart } from "react-icons/fi";
-import productImage from "../../assets/product-image-test.png";
+import { useState, useEffect, useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import 'swiper/css';
 function ProductsSlider() {
+    const swiperRef = useRef(null);
+
+    const [allProducts, setAllProducts] = useState([]);
+    const getAllProducts = async () => {
+        try {
+            const res = await fetch(`https://dummyjson.com/products?limit=200`);
+
+            const data = await res.json();
+
+            setAllProducts(data.products);
+        }
+        catch {
+            console.error("Product not found data");
+        }
+    }
+
+    useEffect(() => {
+        getAllProducts()
+    }, []);
+
+    const allPhones = allProducts.filter((product) => product.category == "smartphones");
+    const allmobileAccessories = allProducts.filter((product) => product.category == "mobile-accessories");
+    const allLaptops = allProducts.filter((product) => product.category == "laptops");
+    const allSliderProductsCart = [...allPhones, ...allmobileAccessories, ...allLaptops].sort((a, b) => b.price - a.price).slice(5, 15);
+
     return (
         <section className="w-full h-full products-slider mb-[72px]">
             <div className="container min-w-[100%] h-full">
                 <div className="flex items-center justify-between w-full h-full col-heading">
                     <h2 className="text-sm font-bold uppercase text-textcolorPrimary">You might also like</h2>
                     <div className="flex next-prev-btns border-[1px] border-textColor w-[85px] h-9  pl-3 pr-3 rounded-[4px] justify-center items-center">
-                        <div className="prev-btn border-r-[1px] border-textColor h-full flex justify-center items-center w-[50%] cursor-pointer">
+                        <div className="prev-btn border-r-[1px] border-textColor h-full flex justify-center items-center w-[50%] cursor-pointer"
+                            onClick={() => swiperRef.current.slidePrev()}
+                        >
                             <FaCaretLeft className="text-xl transition-colors duration-500 hover:text-textColor" />
                         </div>
-                        <div className="next-btn w-[50%] flex justify-center items-center cursor-pointer">
+                        <div className="next-btn w-[50%] flex justify-center items-center cursor-pointer"
+                            onClick={() => swiperRef.current.slideNext()}
+                        >
                             <FaCaretRight className="text-xl transition-colors duration-500 hover:text-textColor" />
                         </div>
                     </div>
                 </div>
-                <div className="all-products">
-                    <div className="flex gap-3 col-product">
-                        <div className="col-image-product w-[90px] h-[120px] shadow-lg">
-                            <img src={productImage} alt="product-img" className="object-contain w-full h-full" />
-                        </div>
-                        <div className="flex flex-col gap-3 information-product">
-                            <p className="text-sm uppercase title-product text-textcolorPrimary">SHORT PRINTED DRESS</p>
-                            <div className="flex justify-between content-price-rates">
-                                <span className="text-sm font-bold price-product text-redcolor">₹69.99</span>
-                                <span className="rate-product w-[32px] h-[15px] bg-redcolor text-center text-whiteColor text-[8px] flex justify-center items-center">- 40%</span>
-                            </div>
-                            <button className="add-to-cart flex items-center w-full border-[1px] border-borderColor h-[36px] rounded-[4px] justify-center gap-3 cursor-pointer">
-                                <FiShoppingCart />
-                                <p className="text-sm font-bold text-textcolorPrimary">Add to cart</p>
-                            </button>
-                        </div>
-                    </div>
+                <div className="mt-5 all-products">
+                    <Swiper
+                        modules={[Autoplay]}
+                        slidesPerView={4}
+                        spaceBetween={20}
+                        speed={500}
+                        loop={true}
+                        autoplay={
+                            { delay: 2000, pauseOnMouseEnter: true }
+                        }
+                        onSwiper={(swiper) => swiperRef.current = swiper}
+                        breakpoints={{
+                            450: { slidesPerView: 1 },
+                            600: { slidesPerView: 1 },
+                            650: { slidesPerView: 2 },
+                            992: { slidesPerView: 2 },
+                            1400: { slidesPerView: 3 },
+                            1600: { slidesPerView: 4 }
+                        }}
+                    >
+                        {allSliderProductsCart.length === 0 ?
+                            <>
+                                <h1>loading data products...</h1>
+                            </>
+                            :
+                            allSliderProductsCart.map((product, index) => {
+                                return (
+                                    <SwiperSlide key={product.id}>
+                                        <div className="flex gap-3 col-product w-[300px]">
+                                            <div className="col-image-product w-[90px] h-[100px] bg-gray-100">
+                                                <img src={product.images[0]} alt="product-img" className="object-contain w-full h-full" />
+                                            </div>
+                                            <div className="flex flex-col gap-3 information-product w-[200px]">
+                                                <p className="text-sm uppercase title-product text-textcolorPrimary">{product.title.slice(0, 20)}</p>
+                                                <div className="flex justify-between content-price-rates">
+                                                    <span className="text-sm font-bold price-product text-redcolor">{`₹${product.price}`}</span>
+                                                    <span className="rate-product w-[32px] h-[15px] bg-redcolor text-center text-whiteColor text-[8px] flex justify-center items-center">{product.rating}</span>
+                                                </div>
+                                                <button className="add-to-cart flex items-center w-full border-[1px] border-borderColor h-[36px] rounded-[4px] justify-center gap-3 cursor-pointer">
+                                                    <FiShoppingCart />
+                                                    <p className="text-sm font-bold text-textcolorPrimary">Add to cart</p>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </SwiperSlide>
+                                )
+                            })
+                        }
+                    </Swiper>
                 </div>
             </div>
         </section>
